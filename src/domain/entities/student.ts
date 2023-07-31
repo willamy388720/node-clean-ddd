26ff3d0193f000db3a96f60fabf9c18e25226095
willamy388720 <willamygt@gmail.com>
2023-07-31 11:55:24 -0300

    first commit

---
+++ src/domain/entities/student.ts
@@ -0,0 +1,14 @@
+import { Entity } from "../../core/entities/entity";
+import { UniqueEntityId } from "../../core/entities/unique-entity-id";
+
+interface StudentProps {
+  name: string;
+}
+
+export class Student extends Entity<StudentProps> {
+  static create(props: StudentProps, id?: UniqueEntityId) {
+    const question = new Student({ ...props, createdAt: new Date() }, id);
+
+    return question;
+  }
+}
